6d853d9c968cc9d2a9cf3cb694b1f14e3c3c3a1a
bspence205 <77554087+bspence205@users.noreply.github.com>
2021-05-08 23:35:15 -0700

    Add files via upload

---
+++ battle.js
@@ -0,0 +1,40 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var guess;
+var guesses = 0;
+var hits = 0;
+var isSunk = false;
+
+while (isSunk == false) {
+  guess = prompt("Fire away! enter a value 0-6");
+  if (guess < 0 || guess > 6) {
+    alert("Please enter a valid number");
+  } else {
+    guesses = guesses + 1;
+    if (guess == location1 || guess == location2 || guess == location3) {
+      alert("HIT");
+      hits = hits + 1;
+
+      if (hits == 3) {
+        isSunk = true;
+        alert("You sank my battleship");
+      }
+    } else {
+      alert("MISS");
+    }
+  }
+}
+
+var stats =
+  "You took" +
+  " " +
+  guesses +
+  " " +
+  "to sink my battleship," +
+  " " +
+  "which means your accuracy was" +
+  " " +
+  3 / guesses;
+alert(stats);
